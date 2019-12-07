0cc42c9776e7a635285f685e79e250bbb91dccac
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 14:43:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,9 @@ def load_library(filepath)
   
   # Add keys and values to get_emoticon hash
   all_emoticons.map do |values|
-    pp values[1][0]
+    english_emoticon = values[1][0]
+    japanese_emoticon = values[1][1]
+    emoticons[:get_emoticon][english_emoticon] = japanese_emoticon
   end
   
   
