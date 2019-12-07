32c9b4f712e2a025c966148dc185992634673e41
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 14:51:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ end
 def get_japanese_emoticon(filepath,emoticon)
   emoticons = load_library(filepath)
   lookup_value = emoticons[:get_emoticon][emoticon]
-  lookup_value == nil ? return "Sorry, that emoticon was not found" : return lookup_value
+  lookup_value == nil ? (return "Sorry, that emoticon was not found") : (return lookup_value)
 end
 
 def get_english_meaning
